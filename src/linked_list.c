9e1630e58db1bc422d4286b6290a01dfab63a92a
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-05-04 23:54:31 +0530

    Added some basic Linked List functions

---
+++ src/linked_list.c
@@ -0,0 +1,99 @@
+#include "linked_list.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+Node *create_node(void *data) {
+    // TODO: check whether malloc returned a NULL
+    Node *new_node = malloc(sizeof(Node));
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
+}
+
+void free_node(Node *node) {
+    if (node == NULL) return;
+    free(node);
+}
+
+LinkedList *ll_init() {
+    LinkedList *new_list = malloc(sizeof(LinkedList));
+    new_list->head = NULL;
+    new_list->size = 0;
+}
+
+void ll_print(LinkedList *list, NodePrinterFn *node_printer) {
+    Node *temp = list->head;
+    while (temp != NULL) {
+        node_printer(temp);
+        temp = temp->next;
+    }
+}
+
+int ll_is_empty(LinkedList *list) {
+    return list->size == 0;
+}
+
+void ll_append(LinkedList *list, Node *node) {
+    if (list->head == NULL) {
+        list->head = node;
+        list->size++;
+        return;
+    }
+
+    Node *temp = list->head;
+    while (temp->next != NULL) temp = temp->next;
+    temp->next = node;
+    list->size++;
+}
+
+Node *ll_pop(LinkedList *list) {
+    if (list->size == 1) {
+        Node *temp = list->head;
+        list->head = NULL;
+        list->size--;
+        return temp;
+    }
+    Node *prev;
+    Node *curr = list->head;
+
+    while (curr->next != NULL) {
+        prev = curr;
+        curr = curr->next;
+    }
+
+    prev->next = NULL;
+    list->size--;
+    return curr;
+}
+
+void ll_empty(LinkedList *list) {
+}
+
+void ll_free(LinkedList *list) {
+    Node *temp;
+    while (list->head != NULL) {
+        temp = list->head;
+        list->head = list->head->next;
+        free_node(temp);
+    }
+    free(list);
+}
+
+// LLIter *ll_iter_init(LinkedList *list) {
+//     LLIter *new_iter = malloc(sizeof(LLIter));
+//     new_iter->curr = list->head;
+//     return new_iter;
+// }
+
+// Node *ll_iter_next(LLIter *iterator) {
+//     if (iterator->curr == NULL) return NULL;
+//     Node *curr_val = iterator->curr;
+//     iterator->curr = iterator->curr->next;
+//     return curr_val;
+// }
+
+// void ll_iter_free(LLIter *iterator) {
+//     if (iterator == NULL) return;
+//     free(iterator);
+// }
---
+++ src/linked_list.h
@@ -0,0 +1,37 @@
+#ifndef _LINKED_LIST_H
+#define _LINKED_LIST_H
+
+#include <stdio.h>
+
+typedef struct Node {
+    void *data;
+    struct Node *next;
+} Node;
+
+typedef struct {
+    Node *head;
+    size_t size;
+} LinkedList;
+
+// typedef struct {
+//     Node *curr;
+// } LLIter;
+
+typedef void NodePrinterFn(Node *);
+
+Node *create_node(void *);
+void free_node(Node *);
+
+LinkedList *ll_init();
+void ll_print(LinkedList *, NodePrinterFn *);
+int ll_is_empty(LinkedList *);
+void ll_append(LinkedList *, Node *);
+Node *ll_pop(LinkedList *);
+void ll_empty(LinkedList *);
+void ll_free(LinkedList *);
+
+// LLIter *ll_iter_init(LinkedList *);
+// void ll_iter_free(LLIter *);
+// Node *ll_iter_next(LLIter *);
+
+#endif
