16dfa361fec70a691c22dec39ac788469aabe066
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-05-05 22:42:29 +0530

    Added a TreeNode structure

---
+++ src/eval_utils.c
@@ -0,0 +1,43 @@
+#include "eval_utils.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+TreeNode *_create_float_tree_node(double val) {
+    TreeNode *new_tree_node = malloc(sizeof(TreeNode));
+    TreeNodeData *new_tree_node_data = malloc(sizeof(TreeNodeData));
+
+    new_tree_node_data->float_val = val;
+    new_tree_node_data->type = FLOAT;
+    new_tree_node_data->string_val = NULL;
+
+    new_tree_node->data = new_tree_node_data;
+    new_tree_node->left = NULL;
+    new_tree_node->right = NULL;
+    return new_tree_node;
+}
+
+TreeNode *_create_string_tree_node(char *val) {
+    TreeNode *new_tree_node = malloc(sizeof(TreeNode));
+    TreeNodeData *new_tree_node_data = malloc(sizeof(TreeNodeData));
+
+    new_tree_node_data->string_val = val;
+    new_tree_node_data->type = STRING;
+    new_tree_node_data->float_val = 0.0;
+
+    new_tree_node->data = new_tree_node_data;
+    new_tree_node->left = NULL;
+    new_tree_node->right = NULL;
+    return new_tree_node;
+}
+
+void display_tree_node(TreeNode *node) {
+    if (node->data->type == FLOAT) {
+        printf("%lf\n", node->data->float_val);
+    } else if (node->data->type == STRING) {
+        printf("%s\n", node->data->string_val);
+    } else {
+        fprintf(stderr, "got unknown type\n");
+        exit(1);
+    }
+}
---
+++ src/eval_utils.h
@@ -0,0 +1,40 @@
+#ifndef _EVAL_UTILS_H
+#define _EVAL_UTILS_H
+
+// clang-format off
+#define create_tree_node(data) ({               \
+    _Generic(data,                              \
+        double: _create_float_tree_node,        \
+        char*: _create_string_tree_node         \
+    )(data);                                    \
+})
+
+// clang format on
+
+typedef enum {
+    FLOAT,
+    STRING
+} TreeNodeDataType;
+
+typedef struct TreeNodeData {
+    // the node can store a char * or float
+    // this struct just makes it easier to do thing
+    char *string_val;
+    double float_val;
+    TreeNodeDataType type;
+
+} TreeNodeData;
+
+typedef struct TreeNode {
+    TreeNodeData *data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+} TreeNode;
+
+TreeNode *_create_float_tree_node(double);
+TreeNode *_create_string_tree_node(char *);
+
+void display_tree_node(TreeNode *);
+
+
+#endif
