bfff4a6b7f289dc6d36bc9ab95e1215d7426f307
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-05-09 18:08:02 +0530

    Updated build steps

--- nobuild.c
+++ nobuild.c
@@ -13,6 +13,28 @@ void build_linked_list_so() {
         PATH("build", "lib", "libll.so"));
 }
 
+void build_eval_utils() {
+    INFO("Building eval_utils");
+    MKDIRS("build", "out");
+    CMD("cc", C_FLAGS, "-c",
+        PATH("src", "eval_utils.c"),
+        "-I./src/",
+        "-I./src/lib",
+        "-o",
+        PATH("build", "out", "eval_utils.o"));
+}
+
+void build_eval() {
+    INFO("Building eval");
+    MKDIRS("build", "out");
+    CMD("cc", C_FLAGS, "-c",
+        PATH("src", "eval.c"),
+        "-I./src",
+        "-I./src/lib",
+        "-o",
+        PATH("build", "out", "eval.o"));
+}
+
 void build_main() {
     INFO("Building c_eval");
     MKDIRS("build", "bin");
@@ -21,12 +43,16 @@ void build_main() {
         "-I./src",
         "-I./src/lib",
         PATH("src", "main.c"),
+        PATH("build", "out", "eval_utils.o"),
+        PATH("build", "out", "eval.o"),
         "-lll");
 }
 
 int main(int argc, char **argv) {
     GO_REBUILD_URSELF(argc, argv);
     build_linked_list_so();
+    build_eval_utils();
+    build_eval();
     build_main();
     return 0;
 }
--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-
 cc -o nobuild nobuild.c
 ./nobuild
-exec env LD_LIBRARY_PATH=./build/lib/ ./build/bin/c_eval
+exec env LD_LIBRARY_PATH=./build/lib/ ./build/bin/c_eval "$1"
