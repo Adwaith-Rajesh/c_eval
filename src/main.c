a1ff60345def45ef94ace2608da90e44b16ceb69
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-05-09 22:14:03 +0530

    Removed prints from main.c

--- src/main.c
+++ src/main.c
@@ -6,32 +6,10 @@
 #include "eval_utils.h"
 #include "linked_list.h"
 
-void _test_node_printer(Node *node) {
-    string_print(STRING_P(node->data));
-    printf(" ");
-}
-
-void *_test_free_postfix_list(Node *node, va_list args) {
+void *_free_postfix_list(Node *node, va_list args) {
     (void)args;
     string_destroy(STRING_P(node->data));
     return NULL;
-}
-
-void traverse_tree(TreeNode *node, int level) {
-    if (node == NULL) return;
-
-    for (int i = 0; i < level; i++) {
-        printf((i == level - 1) ? "|-" : " ");
-    }
-
-    if (node->data->type == STRING) {
-        string_print(node->data->string_val);
-    } else if (node->data->type == FLOAT) {
-        printf("%lf", node->data->float_val);
-    }
-    printf("\n");
-    traverse_tree(node->left, level + 1);
-    traverse_tree(node->right, level + 1);
 }
 
 int main(int argc, char **argv) {
@@ -42,19 +20,12 @@ int main(int argc, char **argv) {
 
     String *exp = string_create_from_char_p(argv[1], strlen(argv[1]) + 1);
     LinkedList *p_list = infix_to_postfix_list(exp);
-    ll_print(p_list, _test_node_printer);
-    printf("\n");
-
     TreeNode *tree_node = postfix_list_to_expression_tree(p_list);
-    traverse_tree(tree_node, 0);
+
+    printf("%lf\n", evaluate_expression_tree(tree_node));
 
     string_destroy(exp);
-
-    ll_print(p_list, _test_node_printer);
-    printf("\n");
-    printf("%lf", evaluate_expression_tree(tree_node));
-
-    ll_map(p_list, _test_free_postfix_list);
+    ll_map(p_list, _free_postfix_list);
     ll_free(p_list);
     free_tree_node(tree_node);
 
